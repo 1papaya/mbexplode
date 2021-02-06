be21a15b22d71f000f1663e9781f9f9abbebae9c
1papaya <1love1papaya@gmail.com>
2021-02-06 19:27:34 +0000

    switch to promise-based sqlite3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const Buffer = require("buffer").Buffer;
-const sqlite3 = require("sqlite3");
+const Database = require("sqlite-async");
 const zlib = require("zlib");
-const fs = require("fs");
+const fs = require("fs").promises;
 
-export default mbtilesExploder = function (mbtilesPath, outDir, opts) {
-  // default opts
+const promiseGunzip = require("util").promisify(zlib.gunzip);
+
+export default mbtilesExploder = async function (mbtilesPath, outDir, opts) {
+  // apply default opts
   opts = Object.assign(
     {
       unzipPbf: false,
@@ -14,56 +16,53 @@ export default mbtilesExploder = function (mbtilesPath, outDir, opts) {
     opts
   );
 
-  // open SQL
-  const db = new sqlite3.Database(mbtilesPath, function (err) {
-    throw new Error(err);
-  });
-
-  // get metadata
-  db.all("SELECT name, value FROM metadata", (err, meta) => {
-    if (err) throw new Error(err);
-
-    // get metadata as JSON name:value object
+  return Database.open(mbtilesPath).then(async (db) => {
     const metadata = Object.assign(
       {},
-      ...meta.map((m) => {
+      ...(await db.all("SELECT name, value FROM metadata")).map((m) => {
         return { [t.name]: t.value };
       })
     );
 
-    // if format not specified don't know what to do
+    const zoomLevels = await db.all(
+      "SELECT DISTINCT zoom_level AS z FROM tiles ORDER BY z ASC"
+    );
+
+    const numTiles = await db.get(`SELECT COUNT(*) from tiles`);
+
+    // check mbtiles validity
     if (!("format" in metadata))
       throw new ReferenceError(`tile format not specified in metadata`);
 
-    // figure out outDir from options
-    if (opts.namedSubfolder)
-      if (!("name" in metadata))
-        throw new ReferenceError(`name not specified in metadata`);
-      else outDir = `${outDir}/${metadata.name}`;
+    if (!("name" in metadata))
+      throw new ReferenceError(`name not specified in metadata`);
 
-    if (opts.verbose) console.log(`outDir: ${outDir}`);
+    // add subfolder from metadata name if specified
+    if (opts.namedSubfolder) outDir = `${outDir}/${metadata.name}`;
 
-    db.each("SELECT * FROM tiles", (err, row) => {
-      if (err) throw new Error(err);
+    console.log(
+      `${metadata.name}: ${metadata.format} | ${numTiles} tiles | z${
+        zoomLevels[0]
+      }-${zoomLevels[zoomLevels.length - 1]}`
+    );
+    
+    console.log(`${metadata.name}: outDir: ${outDir}`);
 
+    return db.each("SELECT * FROM tiles").then((row) => {
       const outFolder = `${outDir}/${row.zoom_level}/${row.tile_column}`;
       const outFile = `${outFolder}/${
         (1 << row.zoom_level) - row.tile_row - 1
       }.${metadata.format}`;
 
-      fs.mkdir(outFolder, { recursive: true }, () => {
+      return fs.mkdir(outFolder, { recursive: true }).then(() => {
         if (opts.unzipPbf && metadata.format === "pbf") {
-          zlib.gunzip(new Buffer(row.tile_data), (err, buffer) => {
-            if (err) throw new Error(err);
-
-            fs.writeFile(outFile, buffer, (err) => {
-              if (err) throw new Error(err);
+          return promiseGunzip(new Buffer(row.tile_data)).then((buffer) => {
+            return fs.writeFile(outFile, buffer).then(() => {
               if (opts.verbose) console.log(`wrote ${outFile}`);
             });
           });
         } else {
-          fs.writeFile(outFile, row.tile_data, (err) => {
-            if (err) throw new Error(err);
+          return fs.writeFile(outFile, row.tile_data).then(() => {
             if (opts.verbose) console.log(`wrote ${outFile}`);
           });
         }
