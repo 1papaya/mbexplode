6d704fba9fd8ce75c1af2d9b30e6ed46f2418cee
1papaya <1love1papaya@gmail.com>
2021-02-06 22:08:32 +0000

    move to synchronous with better-sqlite3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
+const Database = require("better-sqlite3");
 const Buffer = require("buffer").Buffer;
-const Database = require("sqlite-async");
 const zlib = require("zlib");
-const fs = require("fs").promises;
+const fs = require("fs");
 
-const promiseGunzip = require("util").promisify(zlib.gunzip);
-
-export default mbtilesExploder = async function (mbtilesPath, outDir, opts) {
+export default mbexplode = function (mbtilesPath, outDir, opts) {
   // apply default opts
   opts = Object.assign(
     {
@@ -16,57 +14,61 @@ export default mbtilesExploder = async function (mbtilesPath, outDir, opts) {
     opts
   );
 
-  return Database.open(mbtilesPath).then(async (db) => {
-    const metadata = Object.assign(
-      {},
-      ...(await db.all("SELECT name, value FROM metadata")).map((m) => {
-        return { [t.name]: t.value };
-      })
-    );
+  const db = new Database(mbtilesPath, { fileMustExist: true });
 
-    const zoomLevels = await db.all(
-      "SELECT DISTINCT zoom_level AS z FROM tiles ORDER BY z ASC"
-    );
-
-    const numTiles = await db.get(`SELECT COUNT(*) from tiles`);
-
-    // check mbtiles validity
-    if (!("format" in metadata))
-      throw new ReferenceError(`tile format not specified in metadata`);
-
-    if (!("name" in metadata))
-      throw new ReferenceError(`name not specified in metadata`);
-
-    // add subfolder from metadata name if specified
-    if (opts.namedSubfolder) outDir = `${outDir}/${metadata.name}`;
-
-    console.log(
-      `${metadata.name}: ${metadata.format} | ${numTiles} tiles | z${
-        zoomLevels[0]
-      }-${zoomLevels[zoomLevels.length - 1]}`
-    );
-    
-    console.log(`${metadata.name}: outDir: ${outDir}`);
-
-    return db.each("SELECT * FROM tiles").then((row) => {
-      const outFolder = `${outDir}/${row.zoom_level}/${row.tile_column}`;
-      const outFile = `${outFolder}/${
-        (1 << row.zoom_level) - row.tile_row - 1
-      }.${metadata.format}`;
-
-      return fs.mkdir(outFolder, { recursive: true }).then(() => {
-        if (opts.unzipPbf && metadata.format === "pbf") {
-          return promiseGunzip(new Buffer(row.tile_data)).then((buffer) => {
-            return fs.writeFile(outFile, buffer).then(() => {
-              if (opts.verbose) console.log(`wrote ${outFile}`);
-            });
-          });
-        } else {
-          return fs.writeFile(outFile, row.tile_data).then(() => {
-            if (opts.verbose) console.log(`wrote ${outFile}`);
-          });
-        }
-      });
+  const metadata = db
+    .prepare("SELECT name, value FROM metadata")
+    .all()
+    .map((m) => {
+      return { [t.name]: t.value };
     });
-  });
+
+  const minMaxZoom = db
+    .prepare("SELECT MIN(zoom_level), MAX(zoom_level) FROM tiles")
+    .all();
+
+  const numTiles = db.prepare(`SELECT COUNT(*) from tiles`).get();
+
+  // check mbtiles validity
+  if (!("format" in metadata))
+    throw new ReferenceError(`tile format not specified in metadata`);
+
+  if (!("name" in metadata))
+    throw new ReferenceError(`name not specified in metadata`);
+
+  // add subfolder from metadata name if specified
+  if (opts.namedSubfolder) outDir = `${outDir}/${metadata.name}`;
+
+  console.log(
+    `${metadata.name}: ${metadata.format} | ${numTiles} tiles | z${minMaxZoom[0]}-${minMaxZoom[1]}`
+  );
+
+  console.log(`${metadata.name}: outDir: ${outDir}`);
+
+  const tiles = db.prepare("SELECT * FROM tiles");
+
+  for (const tile of tiles.iterate()) {
+    const path = {
+      dir: outDir,
+      zoom: tile.zoom_level,
+      x: tile.tile_column,
+      y: (1 << tile.zoom_level) - tile.tile_row - 1,
+      ext: metadata.format,
+    };
+
+    const outFolder = `${path.dir}/${path.zoom}/${path.x}`;
+    const outFile = `${path.y}.${path.ext}`;
+    const outPath = `${outFolder}/${outFile}`;
+
+    // make sure output folder exists
+    if (!fs.existsSync(outFolder)) fs.mkdirSync(outFolder, { recursive: true });
+
+    const buffer =
+      metadata.format === "pbf" && opts.unzipPbf
+        ? zlib.gunzip(new Buffer(row.tile_data))
+        : row.tile_data;
+
+    fs.writeFileSync(outFile, buffer);
+    if (opts.verbose) console.log(`wrote ${path.zoom}/${path.x}/${path.y}`);
+  }
 };
